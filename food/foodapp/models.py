b9633d47f80284ffda07fe40f801db4495e6c5d7
supraja15 <57399778+supraja15@users.noreply.github.com>
2019-12-22 21:30:20 +0530

    Update models.py

--- food/foodapp/models.py
+++ food/foodapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-# Create your models here.
+# Create your models here .
 
 class SouthIndianFood(models.Model):
     name=models.CharField(max_length=200,primary_key=True)
